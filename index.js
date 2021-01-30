03d62489295d8933a0b278a47d70ce0167892631
Des Preston <despreston@gmail.com>
2021-01-30 15:25:57 -0500

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const qs = require('querystring')
+
+async function getPostTitles(month, day) {
+  const url = `https://en.wikipedia.org/wiki/${month}_${day}`
+  const { data } = await axios.get(url)
+  const $ = cheerio.load(data)
+  const holidays = []
+
+  $('#Holidays_and_observances')
+    .parent()
+    .next()
+    .children()
+    .map((i, el) => holidays.push($(el).text()))
+
+  return holidays
+};
+
+module.exports.handler = async (event, ctx, cb) => {
+  try {
+    const params = qs.parse(event.rawQueryString)
+    console.log(`Fetching holidays for ${params.m} ${params.d}`)
+    return await getPostTitles(params.m, params.d)
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
